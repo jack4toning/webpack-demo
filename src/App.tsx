fd3ffce71e47128c0b7447bebea2678e8d92b5d1
Moe <18200388927@163.com>
2022-10-21 23:17:06 +0800

    hello husky

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ interface AppProps {}
 const str = TEST_STR;
 // @ts-ignore: webpack providePlugin
 const id = _.uniqueId('test');
+console.log(a);
 
 const App: FunctionComponent<AppProps> = () => {
   return (
